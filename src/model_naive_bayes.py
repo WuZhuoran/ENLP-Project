e32ffb2115b6a93ebb0a8dec926a5ecd69b93956
WuZhuoran <munoliver007@gmail.com>
2018-04-09 12:01:03 -0400

    feat: update bigram NB model

--- src/model_naive_bayes.py
+++ src/model_naive_bayes.py
@@ -1,83 +1,75 @@
-from sklearn.naive_bayes import MultinomialNB
+import time
+
 import pandas as pd
 import numpy as np
-from sklearn.metrics import accuracy_score
-import time
-from keras.preprocessing import sequence
-from keras.preprocessing.text import Tokenizer
+from sklearn import metrics
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import *
 
 TRAIN_DATA_FILE = 'train_cleaned.tsv'
 
 
 def load_train_data(path):
-    D = pd.read_csv(path, sep='\t', header=0)
+    data = pd.read_csv(path, sep='\t', header=0)
 
-    D['Sentiment'] = D['Sentiment'].map(lambda x: 0 if x == 0 else x)
-    D['Sentiment'] = D['Sentiment'].map(lambda x: 1 if x == 2 else x)
-    D['Sentiment'] = D['Sentiment'].map(lambda x: 2 if x == 4 else x)
+    data['Sentiment'] = data['Sentiment'].map(lambda x: 0 if x == 0 else x)
+    data['Sentiment'] = data['Sentiment'].map(lambda x: 1 if x == 2 else x)
+    data['Sentiment'] = data['Sentiment'].map(lambda x: 2 if x == 4 else x)
 
-    X_train = D[['Phrase', 'Sentiment']]
-    X_train.is_copy = False
-    X_train['Phrase'] = X_train['Phrase'].astype(str)
+    # Remove empty
+    data['Phrase'].replace('', np.nan, inplace=True)
+    data.dropna(subset=['Phrase'], inplace=True)
 
-    X = np.array(list(X_train['Phrase']))
-    Y_train = np.array(list(D['Sentiment']))
+    data['Phrase'] = data['Phrase'].astype(str)
 
-    return X, Y_train
-
-
-def shuffle_2(a, b):  # Shuffles 2 arrays with the same order
-    s = np.arange(a.shape[0])
-    np.random.shuffle(s)
-    return a[s], b[s]
+    return data
 
 
 current_time = time.time()
 
-X_train, Y_train = load_train_data('../data/' + TRAIN_DATA_FILE)
+train = load_train_data('../data/' + TRAIN_DATA_FILE)
 
 load_time = time.time() - current_time
 
 print('Time to Load ' + TRAIN_DATA_FILE + ': ' + str(load_time) + 's')
 
-# Feature Engineering
-Tokenizer = Tokenizer()
-Tokenizer.fit_on_texts(X_train)
-# Tokenizer.fit_on_texts(X_train)
-Tokenizer_vocab_size = len(Tokenizer.word_index) + 1
-print("Vocab size", Tokenizer_vocab_size)
+train_X, test_X, train_y, test_y = train_test_split(train['Phrase'], train['Sentiment'], test_size=0.20)
 
-# masking
-num_test = 1000
-mask = range(num_test)
+bigram_vectorizer = CountVectorizer(analyzer="word",
+                                    tokenizer=None,
+                                    preprocessor=None,
+                                    stop_words=None,
+                                    ngram_range=(2, 2),
+                                    strip_accents='unicode')
 
-Y_Val = Y_train[:num_test]
-Y_Val2 = Y_train[:num_test]
-X_Val = X_train[:num_test]
+bigram_feature_matrix_train = bigram_vectorizer.fit_transform(train_X)
+bigram_feature_matrix_test = bigram_vectorizer.transform(test_X)
 
-X_train = X_train[num_test:]
-Y_train = Y_train[num_test:]
+bigram_multinomialNB_classifier = MultinomialNB().fit(bigram_feature_matrix_train, train_y)
+bigram_multinomialNB_prediction = bigram_multinomialNB_classifier.predict(bigram_feature_matrix_test)
 
-maxWordCount = 60
-maxDictionary_size = Tokenizer_vocab_size
+model = 'Bigram Multinomial Naive Bayes'
+target_names = ['0', '1', '2']
 
-encoded_words = Tokenizer.texts_to_sequences(X_train)
-encoded_words2 = Tokenizer.texts_to_sequences(X_Val)
+print(
+    '-------' + '-' * len(model))
+print(
+    'MODEL:', model)
+print(
+    '-------' + '-' * len(model))
 
-# padding all text to same size
-X_Train_encodedPadded_words = sequence.pad_sequences(encoded_words, maxlen=maxWordCount)
-X_Val_encodedPadded_words = sequence.pad_sequences(encoded_words2, maxlen=maxWordCount)
-
-# shuffling the traing Set
-shuffle_2(X_Train_encodedPadded_words, Y_train)
-
-current_time = time.time()
-
-classifier = MultinomialNB()
-classifier.fit(X_Train_encodedPadded_words, Y_train)
-
-y_pred = classifier.predict(X_Val_encodedPadded_words)
-predictions = [round(value) for value in y_pred]
-accuracy = accuracy_score(Y_Val, predictions)
-
-print("Accuracy: %.2f%%" % (accuracy * 100.0))
+print(
+    'Precision = ' + str(metrics.precision_score(test_y, bigram_multinomialNB_prediction, average=None)))
+print(
+    'Recall = ' + str(metrics.recall_score(test_y, bigram_multinomialNB_prediction, average=None)))
+print(
+    'F1 = ' + str(metrics.f1_score(test_y, bigram_multinomialNB_prediction, average=None)))
+print(
+    'Accuracy = ' + str(metrics.accuracy_score(test_y, bigram_multinomialNB_prediction)))
+print(
+    'Confusion matrix =  \n' + str(
+        metrics.confusion_matrix(test_y, bigram_multinomialNB_prediction, labels=[0, 1, 2])))
+print('\nClassification Report:\n' + classification_report(test_y, bigram_multinomialNB_prediction,
+                                                           target_names=target_names))
