5582b637cc5bd51785425bb895a5dcd8d23c10a2
WuZhuoran <munoliver007@gmail.com>
2018-04-11 19:19:55 -0400

    Zhuoran Wu: update a new CNN config

--- src/model_CNN.py
+++ src/model_CNN.py
@@ -94,6 +94,74 @@ model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accur
 print(model.summary())
 model.fit(X_Train_encodedPadded_words, Y_train, epochs=3, batch_size=32)
 
+'''Another config for CNN
+backend.set_image_dim_ordering('th')
+
+# Number of feature maps (outputs of convolutional layer)
+N_fm = 300
+# kernel size of convolutional layer
+kernel_size = 8
+
+model = Sequential()
+# Embedding layer (lookup table of trainable word vectors)
+model.add(Embedding(input_dim=W.shape[0], 
+                    output_dim=W.shape[1], 
+                    input_length=conv_input_height,
+                    weights=[W], 
+                    W_constraint=unitnorm()))
+# Reshape word vectors from Embedding to tensor format suitable for Convolutional layer
+model.add(Reshape((1, conv_input_height, conv_input_width)))
+
+# first convolutional layer
+model.add(Convolution2D(N_fm, 
+                        kernel_size, 
+                        conv_input_width, 
+                        border_mode='valid', 
+                        W_regularizer=l2(0.0001)))
+# ReLU activation
+model.add(Activation('relu'))
+
+# aggregate data in every feature map to scalar using MAX operation
+model.add(MaxPooling2D(pool_size=(conv_input_height-kernel_size+1, 1)))
+
+model.add(Flatten())
+model.add(Dropout(0.5))
+# Inner Product layer (as in regular neural network, but without non-linear activation function)
+model.add(Dense(2))
+# SoftMax activation; actually, Dense+SoftMax works as Multinomial Logistic Regression
+model.add(Activation('softmax'))
+
+# Custom optimizers could be used, though right now standard adadelta is employed
+opt = Adadelta(lr=1.0, rho=0.95, epsilon=1e-6)
+model.compile(loss='categorical_crossentropy', 
+              optimizer=opt,
+              metrics=['accuracy'])
+              
+epoch = 0
+val_acc = []
+val_auc = []
+
+N_epoch = 3
+
+for i in xrange(N_epoch):
+    model.fit(train_X, train_Y, batch_size=50, nb_epoch=1, verbose=1)
+    output = model.predict_proba(val_X, batch_size=10, verbose=1)
+    # find validation accuracy using the best threshold value t
+    vacc = np.max([np.sum((output[:,1]>t)==(val_Y[:,1]>0.5))*1.0/len(output) for t in np.arange(0.0, 1.0, 0.01)])
+    # find validation AUC
+    vauc = roc_auc_score(val_Y, output)
+    val_acc.append(vacc)
+    val_auc.append(vauc)
+    print 'Epoch {}: validation accuracy = {:.3%}, validation AUC = {:.3%}'.format(epoch, vacc, vauc)
+    epoch += 1
+    
+print '{} epochs passed'.format(epoch)
+print 'Accuracy on validation dataset:'
+print val_acc
+print 'AUC on validation dataset:'
+print val_auc
+'''
+
 # Final evaluation of the model
 scores = model.evaluate(X_Val_encodedPadded_words, Y_Val, verbose=0)
 print("Accuracy: %.2f%%" % (scores[1]*100))
