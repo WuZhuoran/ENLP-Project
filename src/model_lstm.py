344e4c7569e2b8d710311292b54ceb86644fb872
WuZhuoran <munoliver007@gmail.com>
2018-04-23 19:59:03 -0400

    Zhuoran Wu: refact model_all

--- src/model_lstm.py
+++ src/model_lstm.py
@@ -2,23 +2,35 @@ import datetime
 import time
 
 import keras
+import re
 import numpy as np
 import pandas as pd
+from bs4 import BeautifulSoup
+from nltk.corpus import stopwords
 from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, EarlyStopping
 from keras.constraints import maxnorm
-from keras.layers import Embedding, Bidirectional
+from keras.layers import Embedding
 from keras.layers.core import Dense, Dropout
 from keras.layers.recurrent import LSTM
 from keras.models import Sequential
 from keras.optimizers import SGD
 from keras.preprocessing import sequence
 from keras.preprocessing.text import Tokenizer
+from gensim.models import KeyedVectors
 
 current_time = time.time()
 seed = 19960214
 np.random.seed(seed)
 TRAIN_DATA_FILE = 'train_mapped.tsv'
 TEST_DATA_FILE = 'test.tsv'
+model_name = "../data/GoogleNews-vectors-negative300.bin"
+
+# Vector_type = {'Word2vec', 'No'}
+vector_type = "No"
+train_list = []
+test_list = []
+word2vec_input = []
+num_features = 300
 
 
 def load_train_data(path):  # loads data , caluclate Mean & subtract it data, gets the COV. Matrix.
@@ -40,6 +52,127 @@ def shuffle_2(a, b):  # Shuffles 2 arrays with the same order
     s = np.arange(a.shape[0])
     np.random.shuffle(s)
     return a[s], b[s]
+
+
+def clean_review(raw_review, remove_stopwords=False, output_format="string"):
+    """
+    Input:
+            raw_review: raw text of a movie review
+            remove_stopwords: a boolean variable to indicate whether to remove stop words
+            output_format: if "string", return a cleaned string
+                           if "list", a list of words extracted from cleaned string.
+    Output:
+            Cleaned string or list.
+    """
+
+    # Remove HTML markup
+    text = BeautifulSoup(raw_review)
+
+    # Keep only characters
+    text = re.sub("[^a-zA-Z]", " ", text.get_text())
+
+    # Split words and store to list
+    text = text.lower().split()
+
+    if remove_stopwords:
+
+        # Use set as it has O(1) lookup time
+        stops = set(stopwords.words("english"))
+        words = [w for w in text if w not in stops]
+
+    else:
+        words = text
+
+    # Return a cleaned string or list
+    if output_format == "string":
+        return " ".join(words)
+
+    elif output_format == "list":
+        return words
+
+
+def review_to_doublelist(review, tokenizer, remove_stopwords=False):
+    """
+    Function which generates a list of lists of words from a review for word2vec uses.
+
+    Input:
+        review: raw text of a movie review
+        tokenizer: tokenizer for sentence parsing
+                   nltk.data.load('tokenizers/punkt/english.pickle')
+        remove_stopwords: a boolean variable to indicate whether to remove stop words
+
+    Output:
+        A list of lists.
+        The outer list consists of all sentences in a review.
+        The inner list consists of all words in a sentence.
+    """
+
+    # Create a list of sentences
+    raw_sentences = tokenizer.tokenize(review.strip())
+    sentence_list = []
+
+    for raw_sentence in raw_sentences:
+        if len(raw_sentence) > 0:
+            sentence_list.append(clean_review(raw_sentence, False, "list"))
+    return sentence_list
+
+
+def review_to_vec(words, model, num_features):
+    """
+    Function which generates a feature vector for the given review.
+
+    Input:
+        words: a list of words extracted from a review
+        model: trained word2vec model
+        num_features: dimension of word2vec vectors
+
+    Output:
+        a numpy array representing the review
+    """
+
+    feature_vec = np.zeros((num_features), dtype="float32")
+    word_count = 0
+
+    # index2word is a list consisting of all words in the vocabulary
+    # Convert list to set for speed
+    index2word_set = set(model.index2word)
+
+    for word in words:
+        if word in index2word_set:
+            word_count += 1
+            feature_vec += model[word]
+
+    feature_vec /= word_count
+    return feature_vec
+
+
+def gen_review_vecs(reviews, model, num_features):
+    """
+    Function which generates a m-by-n numpy array from all reviews,
+    where m is len(reviews), and n is num_feature
+
+    Input:
+            reviews: a list of lists.
+                     Inner lists are words from each review.
+                     Outer lists consist of all reviews
+            model: trained word2vec model
+            num_feature: dimension of word2vec vectors
+    Output: m-by-n numpy array, where m is len(review) and n is num_feature
+    """
+
+    curr_index = 0
+    review_feature_vecs = np.zeros((len(reviews), num_features), dtype="float32")
+
+    for review in reviews:
+
+        if curr_index % 1000 == 0.:
+            print(
+                "Vectorizing review %d of %d" % (curr_index, len(reviews)))
+
+        review_feature_vecs[curr_index] = review_to_vec(review, model, num_features)
+        curr_index += 1
+
+    return review_feature_vecs
 
 
 X_train, Y_train, feature_names = load_train_data('../data/' + TRAIN_DATA_FILE)
@@ -69,21 +202,44 @@ Y_train = Y_train[num_test:]
 maxWordCount = 60
 maxDictionary_size = Tokenizer_vocab_size
 
-encoded_words = Tokenizer.texts_to_sequences(X_train)
-encoded_words2 = Tokenizer.texts_to_sequences(X_Val)
-encoded_words3 = Tokenizer.texts_to_sequences(X_test)
+# Extract words from reviews
+print('Start Extract Words from Reviews...')
 
-# padding all text to same size
-X_Train_encodedPadded_words = sequence.pad_sequences(encoded_words, maxlen=maxWordCount)
-X_Val_encodedPadded_words = sequence.pad_sequences(encoded_words2, maxlen=maxWordCount)
-X_test_encodedPadded_words = sequence.pad_sequences(encoded_words3, maxlen=maxWordCount)
+# xrange is faster when iterating
+if vector_type == "Word2vec":
+    for i in range(0, len(X_train.Phrase)):
+        train_list.append(clean_review(X_train.Phrase[i], output_format="list"))
+        if i % 1000 == 0:
+            print("Cleaning training review", i)
 
-# One Hot Encoding
-Y_train = keras.utils.to_categorical(Y_train, 5)
-Y_Val = keras.utils.to_categorical(Y_Val, 5)
+    for i in range(0, len(X_Val.Phrase)):
+        test_list.append(clean_review(X_Val.Phrase[i], output_format="list"))
+        if i % 1000 == 0:
+            print("Cleaning test review", i)
+    print("Loading the pre-trained model")
+    model = KeyedVectors.load_word2vec_format(model_name, binary=True)
+    model.init_sims(replace=True)
+    print("Vectorizing training review")
+    train_vec = gen_review_vecs(train_list, model, num_features)
+    print("Vectorizing test review")
+    test_vec = gen_review_vecs(test_list, model, num_features)
+else:
 
-# shuffling the traing Set
-shuffle_2(X_Train_encodedPadded_words, Y_train)
+    encoded_words = Tokenizer.texts_to_sequences(X_train)
+    encoded_words2 = Tokenizer.texts_to_sequences(X_Val)
+    encoded_words3 = Tokenizer.texts_to_sequences(X_test)
+
+    # padding all text to same size
+    X_Train_encodedPadded_words = sequence.pad_sequences(encoded_words, maxlen=maxWordCount)
+    X_Val_encodedPadded_words = sequence.pad_sequences(encoded_words2, maxlen=maxWordCount)
+    X_test_encodedPadded_words = sequence.pad_sequences(encoded_words3, maxlen=maxWordCount)
+
+    # One Hot Encoding
+    Y_train = keras.utils.to_categorical(Y_train, 5)
+    Y_Val = keras.utils.to_categorical(Y_Val, 5)
+
+    # shuffling the traing Set
+    shuffle_2(X_Train_encodedPadded_words, Y_train)
 
 # model
 model = Sequential()
